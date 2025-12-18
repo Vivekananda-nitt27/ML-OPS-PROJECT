07312d36469478b410dc5908f451899edc4c00c8
vivekananda <vivekanandasahoo34@gmail.com>
2025-12-18 10:08:17 +0530

    The cicd commit

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -17,6 +17,11 @@ TEST_FILE_NAME: str = "test.csv"
 FILE_NAME: str = "notebook\data\EasyVisa.csv"
 MODEL_FILE_NAME = "model.pkl"
 
+
+AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
+REGION_NAME = "us-east-1"
+
 TARGET_COLUMN = "case_status"
 CURRENT_YEAR = date.today().year
 PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"
@@ -56,3 +61,13 @@ MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
 MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
 
 
+"""
+MODEL EVALUATION related constant 
+"""
+MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
+MODEL_BUCKET_NAME = "vivek-mlops-usvisa"
+MODEL_PUSHER_S3_KEY = "model-registry"
+
+
+APP_HOST = "0.0.0.0"
+APP_PORT = 8080
